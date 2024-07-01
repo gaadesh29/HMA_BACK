f4ee66b7c006612f40eb73f35ddc07fea966d201
Aadesh <g.aadesh2002@gmail.com>
2024-07-01 16:43:47 +0530

    commit

--- app.js
+++ app.js
@@ -12,31 +12,46 @@ import appointmentRouter from "./router/appointmentRouter.js";
 const app = express();
 config({ path: "./config/config.env" });
 
+// Ensure environment variables are set correctly
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.DASHBOARD_URL];
+
+// Set up CORS middleware
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
 
+// Middleware for parsing cookies and request bodies
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
+// Middleware for handling file uploads
 app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
   })
 );
+
+// Routers
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
 
+// Database connection
 dbConnection();
 
+// Error handling middleware
 app.use(errorMiddleware);
+
 export default app;
--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
+      dbName: "HMA_HOSPITAL",
     })
     .then(() => {
       console.log("Connected to database!");
